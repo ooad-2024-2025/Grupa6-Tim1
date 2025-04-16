d73c6c97a434d90fbcd00dd0e55acb972f942798
Eldar Hadžiselimović <eldarbus33@gmail.com>
2025-04-16 17:01:58 +0200

    Initial commit

---
+++ Revalb/Revalb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Revalb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
