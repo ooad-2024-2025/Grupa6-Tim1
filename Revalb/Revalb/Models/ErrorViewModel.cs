06c9a6fc675383e83668fbf3bc6ad4d6f6463d36
eldarhadzs33 <eldarbus33@gmail.com>
2025-05-21 11:03:23 +0200

    Konacno sve struktuirano

--- Revalb/Revalb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Revalb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
