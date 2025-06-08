5fb4f09bd63edce7a81a3d60fe7b84fc0affb249
eldarhadzs33 <eldarbus33@gmail.com>
2025-06-08 16:01:11 +0200

    Dovrseno, ne skroz ali vecina

--- Revalb/obj/Debug/net8.0/Revalb.AssemblyInfo.cs
+++ Revalb/obj/Debug/net8.0/Revalb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Revalb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfc366d49b38c2f01ae9b1f20ae2b49778b9beb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cce13f3f4a4f374a983d0df2bcb93511d38d57c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Revalb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Revalb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
